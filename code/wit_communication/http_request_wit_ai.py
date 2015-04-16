da874443671a7d37c1cff3f654a7cea8ea3f5ae2
Stef Janssen <steffos.janssen@gmail.com>
2015-04-16 15:47:21 +0200

    Split up conversation managing to another file, is more OO-ish now.

--- code/wit_communication/http_request_wit_ai.py
+++ code/wit_communication/http_request_wit_ai.py
@@ -2,7 +2,6 @@ __author__ = 'stefjanssen'
 
 import wit
 import json
-import speech_synthesis
 
 access_token = 'ONIABRH52NRVRVSZIUHYHWUV6LGWKCP7'
 
@@ -22,7 +21,6 @@ def get_outcome_response(json_response):
                     response["product"] = value2[0]["value"]
         if key == "intent":
             response["intent"] = value
-
     return response
 
 def get_wit_response():
@@ -34,19 +32,8 @@ def get_wit_response():
     response = get_outcome_response(response)
     return response
 
-speech_synthesis.say_something("Ask me something!")
-response = get_wit_response()
 
-recipe = {"flour": "200 grams", "butter": "150 grams", "eggs": "2", "sugar": "150 grams"}
 
-if response["intent"] == "check_amount":
-    if "product" in response:
-        if response["product"] not in recipe:
-            speech_synthesis.say_something("You do not need " + response["product"])
-        else:
-            speech_synthesis.say_something("The amount you need of " + response["product"] + " is " + recipe[response["product"]])
-    else:
-        speech_synthesis.say_something("I did not quite get that, what product did you say?")
 
 
 
