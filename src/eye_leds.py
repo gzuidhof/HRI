27bfab428a5734b310cc21a5c54c523f69462dd3
Stef Janssen <steffos.janssen@gmail.com>
2015-06-19 13:50:32 +0200

    Eye leds fix

--- src/eye_leds.py
+++ src/eye_leds.py
@@ -12,7 +12,7 @@ class LedController:
         self.use_nao = use_nao
 
         if use_nao:
-            leds = ALProxy("ALLeds","10.0.1.3",9559)
+            self.leds = ALProxy("ALLeds","10.0.1.3",9559)
             red_names = [
                 "Face/Led/Red/Left/0Deg/Actuator/Value",
                 "Face/Led/Red/Left/90Deg/Actuator/Value",
@@ -30,7 +30,7 @@ class LedController:
                 "Face/Led/Red/Right/135Deg/Actuator/Value",
                 "Face/Led/Red/Right/225Deg/Actuator/Value",
                 "Face/Led/Red/Right/315Deg/Actuator/Value"]
-            leds.createGroup("RedGroup",red_names)
+            self.leds.createGroup("RedGroup",red_names)
 
             blue_names = [
                 "Face/Led/Blue/Left/0Deg/Actuator/Value",
@@ -49,7 +49,7 @@ class LedController:
                 "Face/Led/Blue/Right/135Deg/Actuator/Value",
                 "Face/Led/Blue/Right/225Deg/Actuator/Value",
                 "Face/Led/Blue/Right/315Deg/Actuator/Value"]
-            leds.createGroup("BlueGroup",blue_names)
+            self.leds.createGroup("BlueGroup",blue_names)
 
             green_names = [
                 "Face/Led/Green/Left/0Deg/Actuator/Value",
@@ -68,13 +68,13 @@ class LedController:
                 "Face/Led/Green/Right/135Deg/Actuator/Value",
                 "Face/Led/Green/Right/225Deg/Actuator/Value",
                 "Face/Led/Green/Right/315Deg/Actuator/Value"]
-            leds.createGroup("GreenGroup",green_names)
+            self.leds.createGroup("GreenGroup",green_names)
 
 
             # Switch the new groups on
-            leds.on("RedGroup")
-            leds.on("BlueGroup")
-            leds.on("GreenGroup")
+            self.leds.on("RedGroup")
+            self.leds.on("BlueGroup")
+            self.leds.on("GreenGroup")
     
     def set_eyes_to_green(self, use_nao):
         if use_nao:
